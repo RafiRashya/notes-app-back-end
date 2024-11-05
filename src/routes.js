67bc3cc284753f9524cc130aa26fddc146741533
RafiRashya <rafi.rashya.22@gmail.com>
2024-11-05 15:11:17 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+/* eslint-disable linebreak-style */
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNotesByIdHandler,
+  editNoteByIdHandler,
+  deleteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNotesByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteByIdHandler,
+  },
+];
+
+module.exports = routes;
